49eaf039b6b74d437738d5030fc5e7628f69662e
TarekS93 <tarek-93@live.it>
2022-04-12 13:46:54 +0000

    Aggiornamento hilla funzionante

---
+++ hilla-todo/vite.config.ts
@@ -0,0 +1,10 @@
+import { UserConfigFn } from 'vite';
+import { overrideVaadinConfig } from './vite.generated';
+
+const customConfig: UserConfigFn = (env) => ({
+  // Here you can add custom Vite parameters
+  // https://vitejs.dev/config/
+  server: { host: '0.0.0.0' },
+});
+
+export default overrideVaadinConfig(customConfig);
