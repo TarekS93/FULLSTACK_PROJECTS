d113db6eb0a05d32e73d332f7817b17d17101245
TarekS93 <tarek-93@live.it>
2022-04-17 09:23:23 +0000

    Aggiornamenti vari

--- hilla-todo/src/main/java/com/example/application/security/MySecurityConfig.java
+++ hilla-todo/src/main/java/com/example/application/security/MySecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,19 +19,23 @@ public class MySecurityConfig extends VaadinWebSecurityConfigurerAdapter {
         // denies all other
         super.configure(http);
         // per autenticazione legacy con form integrata di spring-security
-        // http.formLogin();
+        // http.formLogin().loginPage("/login").permitAll();
         // per autenticazione con vaadin-hilla
         setLoginView(http, "/login");
-
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Configure users and roles in memory
         auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}user").roles("USER")
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user")
+                .password("$2a$12$U/wUAxjIO7WRZkXtRzumauYc3wfBSK5cMjJ3ACPdzpg1QZ8EqRpdW")
+                .roles("USER")
                 .and()
-                .withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
+                .withUser("admin")
+                .password("$2a$12$9Cc.xTaATd8RChEjbOlMjO/3BEVdTxb90Iqvq7s/Y4Kd4Xcd2D9W6")
+                .roles("ADMIN");
     }
 
     @Override
--- hilla-todo/webpack.config.js
+++ hilla-todo/webpack.config.js
@@ -13,12 +13,15 @@ const flowDefaults = require('./webpack.generated.js');
  * To change the webpack config, add a new configuration object in
  * the merge arguments below:
  */
-module.exports = merge(flowDefaults,
+module.exports = merge(
+  flowDefaults,
   // Override default configuration
-  // {
-  //   mode: 'development',
-  //   devtool: 'inline-source-map',
-  // },
+  {
+    devServer: {
+      host: '0.0.0.0',
+      port: 35729,
+    },
+  }
 
   // Add a custom plugin
   // (install the plugin with `npm install --save-dev webpack-bundle-analyzer`)
