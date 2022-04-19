a288d817499b46b56539d9ca2584022c148914ad
TarekS93 <tarek-93@live.it>
2022-04-19 18:15:05 +0000

    Aggiornamento db

--- hilla-todo/src/main/java/com/example/application/security/MySecurityConfig.java
+++ hilla-todo/src/main/java/com/example/application/security/MySecurityConfig.java
@@ -19,52 +19,54 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @KeycloakConfiguration
 public class MySecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth)
+                        throws Exception {
+                KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+                keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+                auth.authenticationProvider(keycloakAuthenticationProvider);
+        }
 
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+        @Bean
+        @Override
+        protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+                return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+        }
 
-    @Override
-    protected void configure(HttpSecurity http)
-            throws Exception {
-        http.httpBasic().disable();
-        http.formLogin().disable();
-        // disable spring security csrf as Vaadin already provides this
-        // also possible to disable this in Vaadin and leave this enabled
-        http.csrf().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/vaadinServlet/UIDL/**").permitAll()
-                .antMatchers("/vaadinServlet/HEARTBEAT/**").permitAll()
-                .anyRequest().authenticated();
-        http
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler())
-                .logoutUrl("/sso/logout").permitAll()
-                .logoutSuccessUrl("/");
-        http
-                .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
-                .addFilterBefore(keycloakAuthenticationProcessingFilter(), BasicAuthenticationFilter.class);
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint());
-        http
-                .sessionManagement()
-                .sessionAuthenticationStrategy(sessionAuthenticationStrategy());
-    }
+        @Override
+        protected void configure(HttpSecurity http)
+                        throws Exception {
+                http.httpBasic().disable();
+                http.formLogin().disable();
+                // disable spring security csrf as Vaadin already provides this
+                // also possible to disable this in Vaadin and leave this enabled
+                http.csrf().disable();
+                http.headers().frameOptions().disable();
+                http
+                                .authorizeRequests()
+                                .antMatchers("/vaadinServlet/UIDL/**").permitAll()
+                                .antMatchers("/vaadinServlet/HEARTBEAT/**").permitAll()
+                                .anyRequest().authenticated();
+                http
+                                .logout()
+                                .addLogoutHandler(keycloakLogoutHandler())
+                                .logoutUrl("/sso/logout").permitAll()
+                                .logoutSuccessUrl("/");
+                http
+                                .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
+                                .addFilterBefore(keycloakAuthenticationProcessingFilter(),
+                                                BasicAuthenticationFilter.class);
+                http
+                                .exceptionHandling()
+                                .authenticationEntryPoint(authenticationEntryPoint());
+                http
+                                .sessionManagement()
+                                .sessionAuthenticationStrategy(sessionAuthenticationStrategy());
+        }
 
-    @Bean
-    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
-    }
+        @Bean
+        public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+                return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+        }
 
 }
